406578cd8eee9830e632b6acb1382e3f22456de9
Yotam Achrak <yotam2585@gmail.com>
2018-08-16 11:40:03 +0300

    GET users hash use`s password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,32 @@
 // const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password ='123abc!';
 
-var token = jwt.sign(data,'secret123');
-console.log(token);
+bcrypt.genSalt(10,(err,salt) => {
+    bcrypt.hash(password,salt,(err,hash) => {
+        //console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token,'secret123');
-console.log('decoded: ',decoded);
+var hashedoassword = '$2a$10$C6lJoHS7WhlpOYpFIA7y4uwYLFVuiVbGQx8Yvb.unI4YVlJqIfVUu';
+
+
+bcrypt.compare(password,hashedoassword,(err,res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data,'secret123');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'secret123');
+// console.log('decoded: ',decoded);
 // var message = 'Im user 3';
 // var hash = SHA256(message).toString();
 // console.log(`Message: ${message}`);
