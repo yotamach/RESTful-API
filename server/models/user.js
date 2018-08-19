7bf1c5e3fb5d8034e2aa20568896c5a500696d50
Yotam Achrak <yotam2585@gmail.com>
2018-08-19 15:53:50 +0300

    POST users login

--- server/models/user.js
+++ server/models/user.js
@@ -71,6 +71,28 @@ UserSchema.statics.findByToken = function(token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email,password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if(!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve,reject) => {
+            bcrypt.compare(password,user.password,(err,res) => {
+               if(res){
+                   resolve(user);
+               }else{
+                   reject();
+               }
+            })
+        });
+    }).catch((e) => {
+        
+    });;
+
+};
+
 UserSchema.pre('save',function (next) {
     var user = this;
 
