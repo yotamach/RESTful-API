e3fa2b2ab663edc60996549c65ba496c25694bc7
Yotam Achrak <yotam2585@gmail.com>
2018-08-28 09:14:06 +0300

    update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -67,7 +67,7 @@ UserSchema.statics.findByToken = function (token) {
   var decoded;
 
   try {
-    decoded = jwt.verify(token, 'abc123');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     return Promise.reject();
   }
